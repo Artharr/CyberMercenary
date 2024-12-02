9b61e0359501005c51ee15e0c1f1336c9f4210dc
Artharr <arabczak16@gmail.com>
2024-12-02 16:38:04 +0100

    player height changes

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using Unity.Mathematics.Geometry;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,14 +8,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private CharacterController playerController;
     [SerializeField] private InputAction moveAction;
     [SerializeField] private GameObject playerMesh;
+    [SerializeField] private LayerMask layerMask;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float turnSpeed;
+    float capsuleHeight = 0f;
     void Start()
     {
         moveAction.Enable();
+        capsuleHeight = playerController.height/2;
     }
-
-    // Update is called once per frame
     void Update()
     {
         Vector2 inputDir = moveAction.ReadValue<Vector2>();
@@ -22,9 +25,18 @@ public class PlayerMovement : MonoBehaviour
         if (moveDir != Vector3.zero)
         {
             playerMesh.transform.rotation = Quaternion.Lerp(
-                                                            playerMesh.transform.rotation,
-                                                            Quaternion.LookRotation(moveDir, Vector3.up),
-                                                            turnSpeed);
+                                                playerMesh.transform.rotation,
+                                                Quaternion.LookRotation(moveDir, Vector3.up),
+                                                turnSpeed);
+        }
+        bool hit = Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitInfo,Mathf.Infinity);
+        if (hit)
+        {
+            transform.position = Vector3.Lerp(
+                                    transform.position,
+                                    new Vector3(transform.position.x, hitInfo.point.y + capsuleHeight, transform.position.z),
+                                    0.5f
+                                    );
         }
     }
 }

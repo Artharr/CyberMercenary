cf9a1ca98017613feef789fcf8eda94c58e67163
Artharr <arabczak16@gmail.com>
2024-12-17 00:21:49 +0100

    bullet speed up

--- Assets/Weapons/Bullets/Bullet/BulletScript.cs
+++ Assets/Weapons/Bullets/Bullet/BulletScript.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    private float speed = 10f;
+    [SerializeField]private float speed = 25f;
     private float lifetime = 5f;
     public float damage = 20f;
     void Start()

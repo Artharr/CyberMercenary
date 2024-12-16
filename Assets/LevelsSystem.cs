73d03c6320c8868029bfe7f8d52126809f82e9a7
Artharr <arabczak16@gmail.com>
2024-12-17 00:21:06 +0100

    level 1 loading

--- Assets/LevelsSystem.cs
+++ Assets/LevelsSystem.cs
@@ -1,33 +1,37 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelsSystem : MonoBehaviour
 {
     private GameData gameData;
-    private Button Level1Button;
-    private Button Level2Button;
-    private Button Level3Button;
-    private Button Level4Button;
-    private Button Level5Button;
-    private Button Level6Button;
-    private Button Level7Button;
+    [SerializeField]private Button Level1Button;
+    [SerializeField] private Button Level2Button;
+    [SerializeField] private Button Level3Button;
+    [SerializeField] private Button Level4Button;
+    [SerializeField] private Button Level5Button;
+    [SerializeField] private Button Level6Button;
+    [SerializeField] private Button Level7Button;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SaveSystem.LoadGame(gameData);
+        //SaveSystem.LoadGame(gameData);
 
-        if (gameData.isLevel7Unlocked)
+        //if (gameData.isLevel7Unlocked)
+        //{
+        //    Level1Button.interactable = true;
+        //    Level2Button.interactable = true;
+        //    Level3Button.interactable = true;
+        //    Level4Button.interactable = true;
+        //    Level5Button.interactable = true;
+        //    Level6Button.interactable = true;
+        //    Level7Button.interactable = true;
+        //}
+        Level1Button.onClick.AddListener(() =>
         {
-            Level1Button.interactable = true;
-            Level2Button.interactable = true;
-            Level3Button.interactable = true;
-            Level4Button.interactable = true;
-            Level5Button.interactable = true;
-            Level6Button.interactable = true;
-            Level7Button.interactable = true;
-        }
+            SceneManager.LoadScene("Level_1");
+        });
     }
 
     

d8e0f2ee08e0829bf5a31a0049f5519296640e62
Matenn <marcingtr123@gmail.com>
2025-01-25 22:36:37 +0100

    menu ui

--- Assets/BrightnessManager.cs
+++ Assets/BrightnessManager.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class BrightnessManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static BrightnessManager Instance { get; private set; }
+
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // Usuń duplikaty
+            Debug.LogWarning("Duplikat BrightnessManager został usunięty.");
+            return;
+        }
+
+        Instance = this; // Ustaw instancję singletonu
         DontDestroyOnLoad(this.gameObject); // Zachowaj obiekt przy zmianie sceny
-        Debug.Log("Obiekt został zachowany: " + gameObject.name);
+        Debug.Log("BrightnessManager singleton został zainicjalizowany: " + gameObject.name);
     }
 }
